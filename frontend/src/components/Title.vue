0fde428c74840de210bc0f43a8b7b9d94d02cc43
wigbald <wigbald@blackhat.com>
2020-08-24 23:03:42 +0200

    linear gradient poc

--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -1,12 +1,17 @@
 <template>
- <v-container class="headshot headshot-1">
+ <v-container >
+  <div class="headshot headshot-1 dot" v-bind:class="{'dot-25':twenty, 'dot-50':fifty, 'dot-75':seventy,'dot-100':hundred}">
 <v-row justify="space-around">
-<div v-for="word in titleTab" :key="word" >
-<span class="letter" :data-letter="word">{{word}}</span>
 <v-spacer class="space"></v-spacer>
-</div>
+<span class="letter" >{{title}}</span>
+<v-spacer class="space"></v-spacer>
+
 </v-row>
+  </div>
+  <div> <v-btn color=teal v-on:click="click()"> {{count}}</v-btn></div>
+  <div class="ma-4"> <v-btn color=teal v-on:click="click0()"> zero</v-btn></div>
 </v-container>
+
 </template>
 <script>
 //mport { mapGetters, mapState } from "vuex";
@@ -17,17 +22,41 @@ export default {
       title: String
   },
   data: () => ({
-  
+  hundred : false,
+  seventy : false,
+  fifty : false,
+  twenty : false,
+  count: 0,
   }),
   computed: {
-    titleTab : function () {
-        
-       return  this.title.split(' ');
-
-  }
+  
   },
 
   methods: {
+    click(){
+      this.count = this.count + 25 ;
+      if (this.count >= 100 ){
+        
+        this.hundred = true;
+        }else if (this.count >= 75){
+        
+         this.seventy = true;
+      }else if (this.count >= 50){
+       
+        this.fifty =true ;
+      }else if (this.count >= 25 ){
+        this.twenty = true;
+      }
+      
+    },
+     click0(){
+      this.count = 0 ;
+      this.fifty = false;
+      this.hundred = false;
+       this.seventy = false;
+       this.twenty=false;
+
+    }
     
   }
 };
@@ -37,6 +66,42 @@ export default {
 body{
   font-family: 'Lato', sans-serif;
     
+}
+
+.dot {
+  border: 1px;  
+  border-style: solid;
+  border-radius: 50%;
+  border-color: black;
+  color: black;
+  padding: 5px;
+  background-size: 200% 200%;
+
+}
+.dot-25 {
+  background-image: 
+    linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 25%;
+  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+}
+.dot-50 {
+  background-image: 
+    linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 50%;
+  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+}
+
+.dot-75 {
+  background-image: 
+    linear-gradient(to top, green 51%, transparent 50%);
+  background-position: 0 75%;
+  transition: background-position 3000, color 3000ms ease, border-color 3000ms ease;
+}
+.dot-100 {
+  background-image: 
+    linear-gradient(to top, green 51%, transparent 50%);
+  background-position: 0 100%;
+  transition: background-position 3000, color 3000ms ease, border-color 3000ms ease;
 }
 .headshot {
   box-shadow: inset  0 0 25px  white,  0 0 45px #009688 ;
@@ -59,57 +124,13 @@ body{
   font-size: 2em;
   margin: 0;
   position: relative;
-  color: #009688;
+  color: white;
   transform-style: preserve-3d;
   perspective: 400;
   z-index: 1;
 }
-.letter:before, .letter:after{
-  position:absolute;
-  content: attr(data-letter);
-  transform-origin: top left;
-  top:0;
-  left:0;
-}
-.letter, .letter:before, .letter:after{
-  transition: all 0.3s ease-in-out;
-}
-.letter:before{
-  color: #fff;
-  text-shadow: 
-    -1px 0px 1px rgba(255,255,255,.8),
-    1px 0px 1px rgba(0,0,0,.8);
-  z-index: 3;
-  transform:
-    rotateX(0deg)
-    rotateY(-15deg)
-    rotateZ(0deg);
-}
-.letter:after{
-  color: rgba(0,0,0,.11);
-  z-index:2;
-  transform:
-    scale(1.08,1)
-    rotateX(0deg)
-    rotateY(0deg)
-    rotateZ(0deg)
-    skew(0deg,1deg);
-}
-.letter:hover:before{
-  color: #fafafa;
-  transform:
-    rotateX(0deg)
-    rotateY(-40deg)
-    rotateZ(0deg);
-}
-.letter:hover:after{
-  transform:
-    scale(1.08,1)
-    rotateX(0deg)
-    rotateY(40deg)
-    rotateZ(0deg)
-    skew(0deg,22deg);
-}
+
+
 </style>
 
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <v-flex>
- <TitleVue title="WELCOME" />
+ <TitleVue title="WELCOME"  />
  
 </v-flex>
 </template>
@@ -21,7 +21,22 @@ export default {
 };
 </script>
 <style scoped>
-
+.dot-50:hover {
+  color: white;
+  border-color: black;
+  background-image: 
+    linear-gradient(to half, #00FA9A 51%, transparent 50%);
+  background-position: 0 50%;
+  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+}
+.dot-80 {
+  color: white;
+  border-color: black;
+  background-image: 
+    linear-gradient(to half, #00FA9A 51%, transparent 50%);
+  background-position: 5 80%;
+  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+}
 </style>
 
 
