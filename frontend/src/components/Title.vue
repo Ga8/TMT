9ab9aaa68d18a739c627461f45a0b06804472327
Ga8 <guibaud.gaetan@hotmail.fr>
2020-08-25 18:54:57 +0200

    front update awesome button

--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -1,17 +1,21 @@
 <template>
- <v-container >
-  <div class="headshot headshot-1 dot" v-bind:class="{'dot-25':twenty, 'dot-50':fifty, 'dot-75':seventy,'dot-100':hundred}">
-<v-row justify="space-around">
-<v-spacer class="space"></v-spacer>
-<span class="letter" >{{title}}</span>
-<v-spacer class="space"></v-spacer>
+  <v-container>
+    <v-btn :disabled="percentage < 80 ">
+      <div
+        class="headshot headshot-1 dot"
+       
+         
+          v-bind:class="classObject" >
 
-</v-row>
-  </div>
-  <div> <v-btn color=teal v-on:click="click()"> {{count}}</v-btn></div>
-  <div class="ma-4"> <v-btn color=teal v-on:click="click0()"> zero</v-btn></div>
-</v-container>
-
+        <v-row justify="space-around">
+          <v-spacer class="space"></v-spacer>
+          <span class="letter">{{title}}</span>
+          <v-spacer class="space"></v-spacer>
+        </v-row>
+      </div>
+    </v-btn>
+  
+  </v-container>
 </template>
 <script>
 //mport { mapGetters, mapState } from "vuex";
@@ -19,106 +23,140 @@
 export default {
   name: "TitleVue",
   props: {
-      title: String
+    title: String,
+    numerateur : Number,
+    denominateur : Number
   },
   data: () => ({
-  hundred : false,
-  seventy : false,
-  fifty : false,
-  twenty : false,
-  count: 0,
+  
   }),
   computed: {
-  
+    percentage : function ()
+    {
+      console.log(this.numerateur / this.denominateur);
+       return 100*(this.numerateur / this.denominateur ); 
+    },
+
+    classObject : function (){
+      console.log(  10*Math.floor(this.percentage/10));
+      return "dot-"+   10*Math.floor(this.percentage/10);
+    }
+    
   },
 
   methods: {
-    click(){
-      this.count = this.count + 25 ;
-      if (this.count >= 100 ){
-        
-        this.hundred = true;
-        }else if (this.count >= 75){
-        
-         this.seventy = true;
-      }else if (this.count >= 50){
-       
-        this.fifty =true ;
-      }else if (this.count >= 25 ){
-        this.twenty = true;
-      }
-      
-    },
-     click0(){
-      this.count = 0 ;
-      this.fifty = false;
-      this.hundred = false;
-       this.seventy = false;
-       this.twenty=false;
-
-    }
-    
-  }
-};
+ 
+}};
 </script>
 <style scoped>
 @import url(https://fonts.googleapis.com/css?family=Lato:900);
-body{
-  font-family: 'Lato', sans-serif;
-    
+body {
+  font-family: "Lato", sans-serif;
 }
 
 .dot {
-  border: 1px;  
+  border: 1px;
   border-style: solid;
   border-radius: 50%;
   border-color: black;
   color: black;
   padding: 5px;
   background-size: 200% 200%;
-
 }
-.dot-25 {
-  background-image: 
-    linear-gradient(to top, green 50%, transparent 50%);
-  background-position: 0 25%;
-  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+.dot-10 {
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 10%;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
+}
+.dot-20 {
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 20%;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
+}
+.dot-30 {
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 30%;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
+}
+.dot-40 {
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 40%;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
 }
 .dot-50 {
-  background-image: 
-    linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
   background-position: 0 50%;
-  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
+}
+.dot-60 {
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 60%;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
+}
+.dot-70 {
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 70%;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
 }
 
-.dot-75 {
-  background-image: 
-    linear-gradient(to top, green 51%, transparent 50%);
-  background-position: 0 75%;
-  transition: background-position 3000, color 3000ms ease, border-color 3000ms ease;
+.dot-80{
+  background-image: linear-gradient(to top, green 51%, transparent 50%);
+  background-position: 0 80%;
+  transition: background-position 3000, color 3000ms ease,
+    border-color 3000ms ease;
+}
+.dot-90 {
+  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-position: 0 90%;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
 }
 .dot-100 {
-  background-image: 
-    linear-gradient(to top, green 51%, transparent 50%);
+  background-image: linear-gradient(to top, green 51%, transparent 50%);
   background-position: 0 100%;
-  transition: background-position 3000, color 3000ms ease, border-color 3000ms ease;
+  transition: background-position 3000, color 3000ms ease,
+    border-color 3000ms ease;
 }
+
+.pulse-button {
+  border: none;
+  box-shadow: 0 0 0 0 lightyellow;
+
+  cursor: pointer;
+  -webkit-animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
+  -moz-animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
+  -ms-animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
+  animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
+}
+.pulse-button:hover {
+  -webkit-animation: none;
+  -moz-animation: none;
+  -ms-animation: none;
+  animation: none;
+}
+
 .headshot {
-  box-shadow: inset  0 0 25px  white,  0 0 45px #009688 ;
-  padding : 45px;
+  box-shadow: inset 0 0 25px white, 0 0 45px #009688;
+  padding: 45px;
   max-width: 400px;
   margin-left: auto;
   margin-top: 30px;
-   margin-bottom: 30px;
+  margin-bottom: 30px;
 }
-.space{
-    min-width: 40px;
+.space {
+  min-width: 40px;
 }
 .headshot-1 {
-	border-radius: 50%;
-
+  border-radius: 50%;
 }
-.letter{
+.letter {
   display: inline-block;
   font-weight: 800;
   font-size: 2em;
@@ -129,8 +167,6 @@ body{
   perspective: 400;
   z-index: 1;
 }
-
-
 </style>
 
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,128 @@
 <template>
-<v-flex>
- <TitleVue title="WELCOME"  />
- 
-</v-flex>
+  <v-flex>
+    <TitleVue title="WELCOME" :numerateur="numerateur" :denominateur="denominateur" />
+    <v-card max-width="800" class="mx-auto mt-10">
+      <v-toolbar color="teal" dark>
+        <v-toolbar-title class="title">Choose a name an a title for your event</v-toolbar-title>
+        <v-tooltip right>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon v-bind="attrs" v-on="on">mdi-comment-question</v-icon>
+          </template>
+          <span>
+            The name and the title
+            <br />are required
+          </span>
+        </v-tooltip>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-text-field
+        label="Title"
+        v-model="title"
+        :counter="20"
+        :rules="[validateTitle]"
+        required
+        outlined
+        class="ma-10"
+        maxlength="20"
+      ></v-text-field>
+      <v-text-field
+        v-model="name"
+        :counter="20"
+        :rules="[validateName]"
+        label="Name"
+        required
+        class="ma-10"
+        outlined
+        maxlength="20"
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :rules="[validateEmail]"
+        label="E-mail"
+        class="ma-10"
+        outlined
+        :counter="50"
+        maxlength="50"
+      ></v-text-field>
+    </v-card>
+  </v-flex>
 </template>
 <script>
-import TitleVue from '../components/Title.vue';
-
+import TitleVue from "../components/Title.vue";
 
 export default {
   data: () => ({
- 
+    email: "",
+    emailValid: false,
+    name: "",
+    nameValid : false,
+    title: "",
+    titleValid : false ,
+    numerateur :0,
+    denominateur : 3
   }),
+  computed: {},
   components: {
-		TitleVue
-	},
+    TitleVue
+  },
   methods: {
-    
+    updateCount() {
+    this.numerateur = 0 ;
+      var arr = [this.emailValid, this.titleValid, this.nameValid];
+      arr.forEach((value) =>{
+        if (value ){
+          this.numerateur++;
+        } 
+      }
+      );
+    },
+    validateTitle() {
+      var result =  this.required(this.title);
+      if (result) {
+         result = this.counter(this.title);
+        if ( this.counter(this.title)) {
+        result = this.counterMin(this.title); 
+          if (result) {
+            this.titleValid =  true;
+          }
+      }
+      }
+      return result;   
+      }
+    ,
+    validateName() {
+      var result =  this.required(this.name);
+      if (result) {
+         result = this.counter(this.name);
+      if ( this.counter(this.name)) {
+        this.nameValid =  true;
+        }
+      }
+      return result;
+      
+      }
+    ,
+    validateEmail() {
+      const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      var result = "Invalid e-mail.";
+      result =  this.required(this.email);
+      if (result && pattern.test(this.email)) {
+        this.emailValid = true ;
+       
+        result = true;
+      }
+       this.updateCount();
+      return result;
+    },
+    required(value) {
+      return !!value || "Required.";
+    },
+    counter( value) {
+     return value.length <= 20 || "Max 20 characters"
+     },
+    counterMin( value) {
+     return value.length > 2 || "Min 2 characters"
+     }
   }
 };
 </script>
@@ -24,18 +130,18 @@ export default {
 .dot-50:hover {
   color: white;
   border-color: black;
-  background-image: 
-    linear-gradient(to half, #00FA9A 51%, transparent 50%);
+  background-image: linear-gradient(to half, #00fa9a 51%, transparent 50%);
   background-position: 0 50%;
-  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
 }
 .dot-80 {
   color: white;
   border-color: black;
-  background-image: 
-    linear-gradient(to half, #00FA9A 51%, transparent 50%);
+  background-image: linear-gradient(to half, #00fa9a 51%, transparent 50%);
   background-position: 5 80%;
-  transition: background-position 3000ms, color 3000ms ease, border-color 3000ms ease;
+  transition: background-position 3000ms, color 3000ms ease,
+    border-color 3000ms ease;
 }
 </style>
 
