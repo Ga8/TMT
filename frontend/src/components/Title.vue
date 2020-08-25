4508e98138c4c548ecee4e22c0d0e98404003f3a
wigbald <wigbald@blackhat.com>
2020-08-25 23:48:38 +0200

    animation awesome button update

--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -1,10 +1,8 @@
 <template>
   <v-container>
-    <v-btn :disabled="percentage < 80 ">
+    <v-btn :disabled="percentage < 80" class="invisible">
       <div
-        class="headshot headshot-1 dot"
-       
-         
+        class="headshot headshot-1 dot "
           v-bind:class="classObject" >
 
         <v-row justify="space-around">
@@ -38,8 +36,12 @@ export default {
     },
 
     classObject : function (){
-      console.log(  10*Math.floor(this.percentage/10));
-      return "dot-"+   10*Math.floor(this.percentage/10);
+    var result =  10*Math.floor(this.percentage/10);
+    if (result >= 80 ){
+      result = result + " pulse"
+    }
+    console.log("dot-"+   result)
+      return "dot-"+   result;
     }
     
   },
@@ -56,86 +58,88 @@ body {
 
 .dot {
   border: 1px;
-  border-style: solid;
   border-radius: 50%;
-  border-color: black;
-  color: black;
   padding: 5px;
   background-size: 200% 200%;
 }
 .dot-10 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 10%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-20 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 20%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-30 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 30%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-40 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 40%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-50 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 50%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-60 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 60%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-70 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 70%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 
 .dot-80{
-  background-image: linear-gradient(to top, green 51%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 51%, transparent 50%);
   background-position: 0 80%;
   transition: background-position 3000, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-90 {
-  background-image: linear-gradient(to top, green 50%, transparent 50%);
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
   background-position: 0 90%;
   transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
 .dot-100 {
-  background-image: linear-gradient(to top, green 51%, transparent 50%);
-  background-position: 0 100%;
-  transition: background-position 3000, color 3000ms ease,
+ 
+  background-image: linear-gradient(to top, rgb(65,184,131) 50%, transparent 50%);
+  background-position: 0 99%;
+  transition: background-position 3000ms, color 3000ms ease,
     border-color 3000ms ease;
 }
-
-.pulse-button {
-  border: none;
-  box-shadow: 0 0 0 0 lightyellow;
-
-  cursor: pointer;
-  -webkit-animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
-  -moz-animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
-  -ms-animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
-  animation: pulse 1.25s infinite cubic-bezier(0.33, 0, 0, 1);
+.pulse{
+  animation: pulse 0.65s infinite cubic-bezier(0.66, 0, 0, 1) alternate;
 }
-.pulse-button:hover {
+
+
+@keyframes pulse { 
+  0%{
+    box-shadow:inset 0 0 25px white, 0px 0px 0 0px #121212, 0px 0px 0 2px  white;
+    opacity :1;
+  }100%{
+    box-shadow: inset 0 0 25px white, 0px 0px 0 0px #121212, 0px 0px  80px red;
+    opacity :1;
+  }
+}
+
+.dot-100:hover {
   -webkit-animation: none;
   -moz-animation: none;
   -ms-animation: none;
@@ -143,7 +147,7 @@ body {
 }
 
 .headshot {
-  box-shadow: inset 0 0 25px white, 0 0 45px #009688;
+  box-shadow: inset 0 0 25px white, 0 0 105px hsl(174, 100%, 29%);
   padding: 45px;
   max-width: 400px;
   margin-left: auto;
@@ -166,6 +170,16 @@ body {
   transform-style: preserve-3d;
   perspective: 400;
   z-index: 1;
+}
+.invisible {
+  margin-bottom: 35px;
+  margin-top: 75px;
+  background-color: none !important;
+  opacity: 100%;
+  width: 0px !important;
+  min-width: 0px !important;
+  padding: 0px !important;
+  height: 0px !important;
 }
 </style>
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -77,27 +77,40 @@ export default {
       );
     },
     validateTitle() {
+      this.titleValid =  false;
       var result =  this.required(this.title);
-      if (result) {
+      if (result===true) {
          result = this.counter(this.title);
-        if ( this.counter(this.title)) {
+        if ( result===true) {
         result = this.counterMin(this.title); 
-          if (result) {
+          if (result ===true) {
             this.titleValid =  true;
           }
       }
       }
+      this.updateCount();
       return result;   
       }
     ,
     validateName() {
+       this.nameValid =  false;
       var result =  this.required(this.name);
-      if (result) {
+       console.log(result);
+      if (result === true) {
+         console.log(result);
          result = this.counter(this.name);
-      if ( this.counter(this.name)) {
+      if (result ===true) {
+         console.log(result);
+         result = this.counterMin(this.name);
+         if (result ===true ){
         this.nameValid =  true;
         }
+        }
       }
+        console.log(result);
+        console.log("name : " +this.name)
+        console.log("counter : " + this.counterMin(this.name))
+       this.updateCount();
       return result;
       
       }
@@ -105,11 +118,15 @@ export default {
     validateEmail() {
       const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       var result = "Invalid e-mail.";
+       this.emailValid = false;
       result =  this.required(this.email);
-      if (result && pattern.test(this.email)) {
+      if (result ===true ) {
+        result =  pattern.test(this.email)
+
+        if (result === true){
         this.emailValid = true ;
        
-        result = true;
+       }
       }
        this.updateCount();
       return result;
